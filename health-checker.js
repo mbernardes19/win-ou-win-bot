156c398df431c0635b9168a00e4b4f401a0ea02b
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-07 19:46:54 -0300

    Fixes health-checker

--- health-checker.js
+++ health-checker.js
@@ -13,20 +13,20 @@ async function runDeploy() {
     }
 }
 
-app.get('/', (req, res) => {
-    console.log('GET /')
-    res.send('Hello WIN WIN').status(200)
-})
-app.get('/revive', async (req, res) => {
-    await runDeploy()
-    res.sendStatus(200);
-})
-
-const PORT = process.env.PORT_TRADER_INFALIVEL_BOT_HEALTH_CHECKER || process.env.PORT_APP || 21571 || 3000
-console.log('PORTA', PORT)
-app.listen(PORT, () => console.log(`Health checker rodando na porta ${PORT}`));
-
 (async () => {
-    const url = await ngrok.connect({auth: '1iVUTWFWa6vXK7PCN9RQjqCcoV5_2A5ZRpKAmpd85izJMNvaa', addr: 21571});
+    const url = await ngrok.connect({auth: '1iZM941vpA0t6pM9Yongcnp6vmS_2FLBEysByJ3ijLvEQo6Tj', addr: 3001});
     console.log(url);
+
+    app.get('/', (req, res) => {
+        console.log('GET /')
+        res.send('Hello WIN WIN').status(200)
+    })
+    app.get('/revive', async (req, res) => {
+        await runDeploy()
+        res.sendStatus(200);
+    })
+    
+    const PORT = process.env.PORT_TRADER_INFALIVEL_BOT_HEALTH_CHECKER || process.env.PORT_APP || 3001
+    console.log('PORTA', PORT)
+    app.listen(PORT, () => console.log(`Health checker rodando na porta ${PORT}`));
 })()
